bd3d58b97517269ff4b26d0e0106a63e5348c213
Dave Rapin <dave@rapin.com>
2010-11-04 15:46:24 -0400

    removed email settings since they aren't being used

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,12 +35,12 @@ config.action_mailer.default_url_options = { :host => 'rapin.com' }
 ENV['PATH'] = "#{ENV['PATH']}:/usr/local/bin"
 ENV['LD_LIBRARY_PATH'] = "#{ENV['LD_LIBRARY_PATH']}:/usr/local/lib"
 
-# gmail settings - TODO edit these if you're not using gmail to send
+# gmail settings - TODO edit these if your want to send email from the application (I've shown gmail settings here)
 config.action_mailer.smtp_settings = {
   :address => "smtp.gmail.com",
   :port => 587,
   :authentication => :plain,
   :enable_starttls_auto => true,
-  :user_name => APP_CONFIG[:system_email],
-  :password => APP_CONFIG[:system_email_pwd]
+  :user_name => 'youremail@gmail.com',
+  :password => 'yourpassword'
 }

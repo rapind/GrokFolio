ea04001b4991390267c716c0f2876c1d98531032
Dave Rapin <dave@rapin.com>
2010-11-08 09:08:51 -0500

    fixed project image pathnames

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,6 @@
 # path where image magic is installed. You may need to change this.
 Paperclip.options[:command_path] = "/usr/local/bin"
+
+Paperclip.interpolates :project_id do |attachment, style|
+  attachment.instance.project_id
+end
